4082ddfbb200661c3249bbc110e09a98b8d9a68d
azim <me@azimgd.com>
2023-10-04 18:58:37 +0500

    implement kernel based process identification, since nsrunningapplication has access to apps residing in current user space

--- macos/scarecrow-communication/holders/ProcessHolder.h
+++ macos/scarecrow-communication/holders/ProcessHolder.h
@@ -9,16 +9,21 @@
 #define ProcessHolder_h
 
 #import <bsm/libbsm.h>
+#import <libproc.h>
+#import <sys/sysctl.h>
 #import <AppKit/AppKit.h>
 
 @interface ProcessHolder : NSObject
 
-@property (nonatomic, copy) NSRunningApplication *runningApplication;
+@property (nonatomic, copy) NSString *path;
+@property (nonatomic, copy) NSString *name;
 
 - (instancetype)init:(audit_token_t *)auditToken;
 
 - (NSDictionary *)payload;
-- (NSRunningApplication *)runningApplicationFromAppAuditToken:(audit_token_t *)auditToken;
+
+- (NSString *)getProcessPathForPID:(pid_t)processID;
+- (NSString *)getProcessNameForPID:(pid_t)processID executablePath:(NSString *)executablePath;
 
 @end
 
--- macos/scarecrow-communication/holders/ProcessHolder.m
+++ macos/scarecrow-communication/holders/ProcessHolder.m
@@ -14,7 +14,8 @@
   self = [super init];
 
   if (self) {
-    _runningApplication = [self runningApplicationFromAppAuditToken:auditToken];
+    _path = [self getProcessPathForPID:audit_token_to_pid(*auditToken)];
+    _name = [self getProcessNameForPID:audit_token_to_pid(*auditToken) executablePath:_path];
   }
 
   return self;
@@ -23,14 +24,141 @@
 - (NSDictionary *)payload
 {
   return @{
-    @"bundleIdentifier": _runningApplication.bundleIdentifier ?: @"",
-    @"localizedName": _runningApplication.localizedName ?: @"",
+    @"bundleIdentifier": _path ?: @"",
+    @"localizedName": _name ?: @"",
   };
 }
 
-- (NSRunningApplication *)runningApplicationFromAppAuditToken:(audit_token_t *)auditToken {
-  pid_t sourceAppPID = audit_token_to_pid(*auditToken);
-  return [NSRunningApplication runningApplicationWithProcessIdentifier:sourceAppPID];
+- (NSString *)getProcessPathForPID:(pid_t)processID {
+  NSString *processPath = getProcessPathFromProc(processID);
+
+  if (processPath == nil) {
+    processPath = getProcessPathUsingSysctl(processID);
+    if (processPath != nil) {
+      processPath = adjustProcessPathWithCurrentWorkingDirectory(processPath, processID);
+    }
+  }
+
+  return processPath;
 }
+
+- (NSString *)getProcessNameForPID:(pid_t)processID executablePath:(NSString *)executablePath
+{
+  NSString *processName = nil;
+  int processNameStatus = -1;
+  char processNameBuffer[PROC_PIDPATHINFO_MAXSIZE] = {0};
+  memset(processNameBuffer, 0x0, sizeof(processNameBuffer));
+
+  if (processID != 0) {
+    processNameStatus = proc_name(processID, &processNameBuffer, sizeof(processNameBuffer));
+    if (processNameStatus >= 0) {
+      processName = [NSString stringWithUTF8String:processNameBuffer];
+    }
+  }
+
+  if (processName == nil) {
+    NSBundle *applicationBundle = findAppBundleAtPath(executablePath);
+    if (applicationBundle != nil) {
+      processName = applicationBundle.infoDictionary[@"CFBundleName"];
+    }
+  }
+
+  if (processName == nil) {
+    processName = [executablePath lastPathComponent];
+  }
+
+  return processName;
+}
+
+
+NSString *getProcessPathFromProc(pid_t processID) {
+  char processPathBuffer[PROC_PIDPATHINFO_MAXSIZE] = {0};
+  int processPathStatus = proc_pidpath(processID, processPathBuffer, sizeof(processPathBuffer));
+
+  if (processPathStatus != 0) {
+    return [NSString stringWithUTF8String:processPathBuffer];
+  }
+
+  return nil;
+}
+
+NSString *getProcessPathUsingSysctl(pid_t processID) {
+  int mib[3] = {CTL_KERN, KERN_ARGMAX, processID};
+  unsigned long systemMaxArgs = 0;
+  size_t size = sizeof(systemMaxArgs);
+
+  if (sysctl(mib, 3, &systemMaxArgs, &size, NULL, 0) != -1) {
+    char *taskArgs = malloc(systemMaxArgs);
+
+    if (taskArgs != NULL) {
+      mib[1] = KERN_PROCARGS2;
+      size = (size_t)systemMaxArgs;
+
+      if (sysctl(mib, 3, taskArgs, &size, NULL, 0) != -1 && size > sizeof(int)) {
+        int numberOfArgs;
+        memcpy(&numberOfArgs, taskArgs, sizeof(numberOfArgs));
+        NSString *processPath = [NSString stringWithUTF8String:taskArgs + sizeof(int)];
+        free(taskArgs);
+        return processPath;
+      }
+
+      free(taskArgs);
+    }
+  }
+
+  return nil;
+}
+
+NSString *adjustProcessPathWithCurrentWorkingDirectory(NSString *processPath, pid_t processID) {
+  if ([processPath hasPrefix:@"./"]) {
+    processPath = [processPath substringFromIndex:2];
+    NSString *currentWorkingDirectory = getProcessCurrentWorkingDirectory(processID);
+    if (currentWorkingDirectory != nil) {
+      processPath = [currentWorkingDirectory stringByAppendingPathComponent:processPath];
+    }
+  }
+
+  return processPath;
+}
+
+NSString *getProcessCurrentWorkingDirectory(pid_t processID)
+{
+  NSString *currentWorkingDirectory = nil;
+  struct proc_vnodepathinfo vnodePathInfo = {0};
+  int vnodePathStatus = -1;
+  memset(&vnodePathInfo, 0x0, sizeof(vnodePathInfo));
+  vnodePathStatus = proc_pidinfo(processID, PROC_PIDVNODEPATHINFO, 0, &vnodePathInfo, sizeof(vnodePathInfo));
+
+  if (vnodePathStatus > 0) {
+    currentWorkingDirectory = [NSString stringWithUTF8String:vnodePathInfo.pvi_cdir.vip_path];
+  }
+
+  return currentWorkingDirectory;
+}
+
+NSBundle *findAppBundleAtPath(NSString *executablePath)
+{
+  NSBundle *applicationBundle = nil;
+  NSString *standardizedPath = [[executablePath stringByStandardizingPath] stringByResolvingSymlinksInPath];
+  NSString *searchPath = standardizedPath;
+
+  while (searchPath && ![searchPath isEqualToString:@"/"] && ![searchPath isEqualToString:@""]) {
+    applicationBundle = [NSBundle bundleWithPath:searchPath];
+
+    if (applicationBundle) {
+      if ([applicationBundle.bundlePath isEqualToString:standardizedPath] ||
+        [applicationBundle.executablePath isEqualToString:standardizedPath]) {
+        break;
+      } else {
+        applicationBundle = nil;
+      }
+    }
+
+    searchPath = [searchPath stringByDeletingLastPathComponent];
+  }
+
+  return applicationBundle;
+}
+
 
 @end
